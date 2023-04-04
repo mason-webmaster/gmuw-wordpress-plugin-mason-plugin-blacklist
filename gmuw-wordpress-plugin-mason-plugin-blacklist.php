cae2dc809e252875df1ad82d86366f24ae856db5
Jan Macario <jmacario@gmu.edu>
2023-04-03 22:12:57 -0400

    initial commit

--- gmuw-wordpress-plugin-mason-plugin-blacklist.php
+++ gmuw-wordpress-plugin-mason-plugin-blacklist.php
@@ -7,7 +7,7 @@
 /**
  * Plugin Name:       Mason WordPress: Plugin blacklist
  * Author:            Mason Web Administration
- * Plugin URI:        
+ * Plugin URI:        https://github.com/mason-webmaster/gmuw-wordpress-plugin-mason-plugin-blacklist
  * Description:       Mason WordPress Plugin to prevent certain plugins from being installed
  * Version:           0.9
  */
@@ -20,15 +20,15 @@ if (!defined('WPINC')) {
 
 // define constants
 	// define list of disallowed plugins. You could define a different list in the wp-config file.
-	if ( ! defined( 'MAT_DISALLOWED_PLUGINS' ) ) {
-		define( 'MAT_DISALLOWED_PLUGINS', [
+	if ( ! defined( 'GMUW_DISALLOWED_PLUGINS' ) ) {
+		define( 'GMUW_DISALLOWED_PLUGINS', [
 			'elementor',
 			'elementor-pro'
 		] );
 	}
 	// define notification email. You could define a different value for this constant in the wp-config file.
-	if ( ! defined( 'MAT_DISALLOWED_PLUGINS_NOTIFICATION_EMAIL' ) ) {
-		define( 'MAT_DISALLOWED_PLUGINS_NOTIFICATION_EMAIL', "jmacario@gmu.edu" );
+	if ( ! defined( 'GMUW_DISALLOWED_PLUGINS_NOTIFICATION_EMAIL' ) ) {
+		define( 'GMUW_DISALLOWED_PLUGINS_NOTIFICATION_EMAIL', "webmaster@gmu.edu" );
 	}
 
 // filter the plugins_api_result to remove listings of plugins matching those on the blacklist
@@ -40,7 +40,7 @@ function gmuw_plugin_api_filter( $res, $action, $arg ) {
 	}
 
 	foreach ( $res->plugins as $i => $plugin ) {
-		if ( in_array( $plugin['slug'], MAT_DISALLOWED_PLUGINS ) ) {
+		if ( in_array( $plugin['slug'], GMUW_DISALLOWED_PLUGINS ) ) {
 			unset( $res->plugins[ $i ] );
 		}
 	}
@@ -52,7 +52,7 @@ function gmuw_plugin_api_filter( $res, $action, $arg ) {
 add_action( 'activate_plugin', 'gmuw_prevent_activation', 1, 2 );
 function gmuw_prevent_activation( $plugin, $network_wide ) {
 	$plug_slug = explode( '/', $plugin );
-	if ( in_array( $plug_slug[0], MAT_DISALLOWED_PLUGINS ) ) {
+	if ( in_array( $plug_slug[0], GMUW_DISALLOWED_PLUGINS ) ) {
 		throw new \Exception();
 	}
 }
@@ -66,13 +66,13 @@ function gmuw_prevent_install_plugins_cap( $allcaps, $caps, $args, $user ) {
 
 	$action = isset( $_REQUEST['action'] ) ? $_REQUEST['action'] : '';
 	$plugin = isset( $_REQUEST['slug'] ) ? trim( $_REQUEST['slug'] ) : '';
-	if ( in_array( $plugin, MAT_DISALLOWED_PLUGINS ) ) {
+	if ( in_array( $plugin, GMUW_DISALLOWED_PLUGINS ) ) {
 		$allcaps['install_plugins'] = 0;
 
 		$username  = $user->data->user_nicename;
 		$useremail = $user->data->user_email;
 		$site      = site_url();
-		$to        = MAT_DISALLOWED_PLUGINS_NOTIFICATION_EMAIL;
+		$to        = GMUW_DISALLOWED_PLUGINS_NOTIFICATION_EMAIL;
 		$subject   = 'Plugin Blacklist Triggered';
 		$message   = "The user $username ($useremail) at $site attempted to install the $plugin, which has been blacklisted ";
 		wp_mail( $to, $subject, $message );
